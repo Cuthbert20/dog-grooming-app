c5dd4f2be75d7b5b6bebfb3c7bf0e7ef568331bb
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-13 10:05:18 -0600

    set up server to listen

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const { SESSION_SECRET, SERVER_PORT, CONNECTION_STRING } = process.env
+const express = require('express')
+const app = express()
+const massive = require('massive')
+const session = require('express-session')
+
+//middleware
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 8
+    }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} is now listening`))
+
+}).catch(err => console.log(err, "not connecting to db stu"))
+
+
