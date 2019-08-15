6b16f083c81fa0fbc368b490a7ac9a2bf7f91057
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-15 09:42:38 -0600

    mapping through array of dogs

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './UpdateUser.css'
+import axios from 'axios'
 
 class UpdateUser extends Component{
     state = {
         dogs: []
     }
     componentDidMount(){
-        
+        //getting the dog api
+        axios.get(`https://api.thedogapi.com/v1/breeds`, {
+            header: {
+               'x-api-key': process.env.REACT_APP_BREED_API_KEY
+            }
+        }).then(res => {
+            // console.log(res.data)
+            this.setState({dogs: res.data})
+        })
     }
     render(){
+        const { dogs } = this.state
+        console.log('dog', dogs)
         return(
             <div>
                 <ul>
-                    
+                    {/* .fitler  */}
+                    {dogs.map((elm) => {
+                        return <li key={elm.id} >{elm.name}</li>
+                    })}
                     <li><input type="text"/></li>
                     <li><input type="text"/></li>
                     <li><input type="text"/></li>
